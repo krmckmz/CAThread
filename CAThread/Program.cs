e72f0b2ac7c17917315c75eb79678e087a749bb9
Kerem Cokmez <kerem.cokmez@medyasoft.com.tr>
2022-04-24 13:00:45 +0300

    Add project files.

---
+++ CAThread/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Thread thread = new Thread(() =>
+    {
+        Thread.Sleep(2000);
+        Console.WriteLine("thread");
+    });
+
+        //thread.Priority = ThreadPriority.BelowNormal;
+        //var isThreadBackground = thread.IsBackground;
+
+        thread.Start();
+        Console.WriteLine(thread.IsAlive);
+
+        //Join komutu async yapı çalışıp işlem bitse dahi tutarlılık sağlamak için
+        //thread'den gelecek sonucu beklemek istediğimiz durumlarda kullanılır.
+        thread.Join();
+        Console.WriteLine("thread");
+        Console.WriteLine(thread.IsAlive);
+    }
+}
